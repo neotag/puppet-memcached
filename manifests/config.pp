71c985e1ada12f232f9a124690fe6ac1e2109771
neotag <hrdcomingatya@gmail.com>
2013-10-06 19:06:01 +0900

    Want to overwrite memcached::config::port by hiera

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,9 +1,10 @@
-class memcached::config {
+class memcached::config (
+  $port       = 21211
+) {
   require boxen::config
 
   $datadir    = "${boxen::config::datadir}/memcached"
   $executable = "${boxen::config::homebrewdir}/bin/memcached"
   $logdir     = "${boxen::config::logdir}/memcached"
   $logfile    = "${logdir}/console.log"
-  $port       = 21211
 }
